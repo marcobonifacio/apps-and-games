92f3574f5f54f5bf4df8245262d9b3231d7ef322
Marco Bonifacio <bonifacio.marco@gmail.com>
2022-02-09 19:15:57 +0100

    Weather ch 2

--- kivy-games/tutorials/weather/main.py
+++ kivy-games/tutorials/weather/main.py
@@ -3,6 +3,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.network.urlrequest import UrlRequest
 import json
+import os
 
 
 class AddLocationForm(BoxLayout):
@@ -11,17 +12,21 @@ class AddLocationForm(BoxLayout):
     
     def search_location(self):
         search_template = 'http://api.openweathermap.org/data/2.5/' + \
-            'find?q={}&type=like'
-        search_url = search_template.format(self.search_input.text)
+            'find?q={}&type=like&appid={}'
+        key = os.environ.get('OpenWeatherKey')
+        search_url = search_template.format(self.search_input.text, key)
         request = UrlRequest(search_url, self.found_location)
 
     def found_location(self, request, data):
         data = json.loads(data.decode()) if not isinstance(data, dict) \
             else data
-        cities = [{'text': '{} ({})'.format(d['name'], d['sys']['country'])} 
-                  for d in data['list']]
-        #self.search_results.data = cities
-        print('/n'.join(cities))
+        cities = [{'text': '{} ({}) - lat.:{} lon.:{}'.format(
+            d['name'], 
+            d['sys']['country'],
+            d['coord']['lat'],
+            d['coord']['lon'])} for d in data['list']]
+        self.search_results.data = cities
+
 
 class WeatherApp(App):
     pass
