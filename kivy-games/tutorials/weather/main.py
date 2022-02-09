eb79e6f3bb74912e4be303746f0ca959ea2ca3ad
Marco Bonifacio <bonifacio.marco@gmail.com>
2022-02-09 18:11:16 +0100

    Weather ch 2

--- kivy-games/tutorials/weather/main.py
+++ kivy-games/tutorials/weather/main.py
@@ -1,5 +1,27 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.network.urlrequest import UrlRequest
+import json
 
+
+class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+    search_results = ObjectProperty()
+    
+    def search_location(self):
+        search_template = 'http://api.openweathermap.org/data/2.5/' + \
+            'find?q={}&type=like'
+        search_url = search_template.format(self.search_input.text)
+        request = UrlRequest(search_url, self.found_location)
+
+    def found_location(self, request, data):
+        data = json.loads(data.decode()) if not isinstance(data, dict) \
+            else data
+        cities = [{'text': '{} ({})'.format(d['name'], d['sys']['country'])} 
+                  for d in data['list']]
+        #self.search_results.data = cities
+        print('/n'.join(cities))
 
 class WeatherApp(App):
     pass
