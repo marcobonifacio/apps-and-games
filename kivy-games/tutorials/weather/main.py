9ab5e2bf4fdca71bdd352e23e9162f499959f260
Marco Bonifacio <bonifacio.marco@gmail.com>
2022-02-22 12:30:53 +0100

    Update weather

--- kivy-games/tutorials/weather/main.py
+++ kivy-games/tutorials/weather/main.py
@@ -1,9 +1,40 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.label import Label
+from kivy.properties import BooleanProperty, ObjectProperty
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.network.urlrequest import UrlRequest
+
 import json
 import os
+
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
+                                 RecycleBoxLayout):
+    pass
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        self.selected = is_selected
 
 
 class AddLocationForm(BoxLayout):
@@ -26,6 +57,10 @@ class AddLocationForm(BoxLayout):
             d['coord']['lat'],
             d['coord']['lon'])} for d in data['list']]
         self.search_results.data = cities
+
+
+class WeatherRoot(BoxLayout):
+    pass
 
 
 class WeatherApp(App):
