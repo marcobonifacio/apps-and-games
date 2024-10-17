bad2a7df825cdbeb1f4bf6d588fc4e36a482e5a2
Marco Bonifacio <bonifacio.marco@gmail.com>
2024-10-17 11:31:23 +0200

    advanced v

---
+++ v/buffered_coroutines.v
@@ -0,0 +1,27 @@
+module main
+
+const count = 4
+
+fn sender(ch chan int) {
+	for i in 0 .. count {
+		ch <- i
+		println('sent value: $i in the channel')
+	}
+}
+
+fn receiver(ch chan int) {
+	for _ in 0 .. count {
+		println('Received value from the channel ${<-ch}')
+	}
+}
+
+fn main() {
+	ch := chan int{cap: 1}
+	defer {
+		ch.close()
+	}
+	t := go receiver(ch)
+	go sender(ch)
+	t.wait()
+	println('End main')
+}
---
+++ v/greet/greet_test.v
@@ -0,0 +1,12 @@
+module main
+
+fn test_greet() {
+	// Arrange
+	name := 'Bob'
+	exp_msg := 'Hello Bob!'
+	// Act
+	act_msg := greet(name)
+	// Assert
+	assert act_msg == exp_msg
+	assert act_msg.contains(name)
+}
